66596774864f6728f6269167ba34e6ccb11e35ca
kristinlashun <141189118+kristinlashun@users.noreply.github.com>
2023-10-13 19:35:23 -0700

    average.py
    average on it own line

--- average.py
+++ average.py
@@ -4,4 +4,6 @@ num_2 = float(input())
 num_3 = float(input())
 num_4 = float(input())
 num_5 = float(input())
-print("The average of those numbers is:", (num_1 + num_2 + num_3 + num_4 + num_5) / 5)
+average = (num_1 + num_2 + num_3 + num_4 + num_5) / 5
+print("The average of those numbers is:")
+print (average)
